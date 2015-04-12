a2b485eecf86f05c11ceee45b19cdb8a182908b6
zegervdv <zegervdv@me.com>
2015-04-12 20:06:42 +0200

    Add Formula for girara

---
+++ girara.rb
@@ -0,0 +1,45 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Girara < Formula
+  homepage "https://pwmt.org/projects/girara/"
+  url "https://pwmt.org/projects/girara/download/girara-0.2.3.tar.gz"
+  sha1 "91a9d55e8dadb053210ee303fa464da63e8031fe"
+  version "0.2.3"
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+  depends_on 'pkg-config'
+  depends_on 'gtk+3'
+  depends_on 'gettext'
+
+  # Patch the makefile for OS X clang
+  # Replaces -soname flag with -install_name
+  patch :p0 do
+    url 'https://github.com/zegervdv/homebrew-zathura/raw/master/girara_Makefile.patch'
+    sha1 '9f004fb607c0b2a4dd9888ff2f4703b60cc17852'
+  end
+
+  def install
+    # Set HOMBREW_PREFIX
+    ENV['DESTDIR'] = prefix
+
+    system "make"
+    system "make install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test girara`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
+
