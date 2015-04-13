cf0512f94f7889ae8eefbeed5d207acd7135fa61
zegervdv <zegervdv@me.com>
2015-04-13 11:27:53 +0200

    Add zathura formula

---
+++ zathura.rb
@@ -0,0 +1,38 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Zathura < Formula
+  homepage "https://pwmt.org/projects/zathura/"
+  url "https://pwmt.org/projects/zathura/download/zathura-0.3.2.tar.gz"
+  version "0.3.2"
+  sha1 "5a08068fbb7210e298636a73aaedce56e267a8eb"
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+  depends_on 'pkg-config'
+  depends_on "libmagic"
+  depends_on "gettext"
+  depends_on "girara"
+
+  def install
+    # Set Homebrew prefix
+    ENV['PREFIX'] = prefix
+    system "make"
+    ENV.deparallelize
+    system "make install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test zathura`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
