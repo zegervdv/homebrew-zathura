b62e661a29e44b29aa6581116c774d6259ad82b1
Zeger Van de Vannet <zegervdv@me.com>
2015-08-28 22:28:24 +0200

    Bump revision for zathura

--- zathura.rb
+++ zathura.rb
@@ -7,6 +7,7 @@ class Zathura < Formula
   url "https://pwmt.org/projects/zathura/download/zathura-0.3.3.tar.gz"
   version "0.3.3"
   sha1 "fc9935b51ee566cbda5978d3f52ab74a4b22c8f4"
+  revision 1
 
   # depends_on "cmake" => :build
   depends_on :x11 # if your formula requires any X11/XQuartz components
