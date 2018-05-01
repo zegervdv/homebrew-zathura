e178a5f4ed0c78801c1c94ec9c17db6f40167075
zegervdv <zegervdv@me.com>
2018-05-01 17:00:11 +0200

    Update to zathura 0.3.9

--- zathura.rb
+++ zathura.rb
@@ -4,9 +4,9 @@
 
 class Zathura < Formula
   homepage "https://pwmt.org/projects/zathura/"
-  url "https://pwmt.org/projects/zathura/download/zathura-0.3.7.tar.gz"
-  version "0.3.7"
-  sha256 "22afff89f4093f22fb82188417ff9bfa9695b19a4fe894dca05b7c821b390ff0"
+  url "https://pwmt.org/projects/zathura/download/zathura-0.3.9.tar.xz"
+  version "0.3.9"
+  sha256 "2f74a5d288db1f46f1d704d69a9c7822985964e90b793636c6c80a25d29f097c"
 
   # depends_on "cmake" => :build
   depends_on :x11 # if your formula requires any X11/XQuartz components
@@ -18,6 +18,8 @@ class Zathura < Formula
   depends_on "glib"
   depends_on "desktop-file-utils"
   depends_on "intltool"
+  depends_on "sphinx-doc"
+  depends_on "meson" => :head
   depends_on "synctex" => :optional
 
   # patch :p0 do
@@ -25,13 +27,17 @@ class Zathura < Formula
   #   sha256 '07c3948280cbe4f757c6c86761845a637092a4e433f53be28362bec3f7fe4237'
   # end
 
+  patch :p0 do
+    url 'https://github.com/zegervdv/homebrew-zathura/raw/master/zathura-meson.patch'
+    sha256 'e63a5cd92e3e4a6aa735714032fe1f7a12a513286f91d162670d09f930aea60c'
+  end
 
   def install
     # Set Homebrew prefix
     ENV['PREFIX'] = prefix
-    system "make"
-    ENV.deparallelize
-    system "make install"
+    system 'mkdir build'
+    system "meson build --prefix #{prefix}"
+    system "cd build && ninja && ninja install"
   end
 
   test do
