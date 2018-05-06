037f31240125e7288c3762d71bacbb7a036f0ee5
zegervdv <zegervdv@me.com>
2018-05-06 10:50:39 +0200

    Add version number to synctex formula

--- synctex.rb
+++ synctex.rb
@@ -1,7 +1,8 @@
 class Synctex < Formula
   homepage "https://github.com/barracks510/synctexdir"
   url "https://github.com/barracks510/synctexdir/archive/master.zip"
-  version "0.0.0"
+  version "1.18.0"
+  sha256 "c643ee9c96b930fdba601814a381557521926b7373336f139d924c3e28a8e5fa"
 
   def install
     system "gcc -D__SYNCTEX_WORK__ -Wall -I. -lz -shared synctex_parser.c synctex_parser_utils.c -o libsynctex.dylib"
@@ -17,7 +18,7 @@ class Synctex < Formula
       s.gsub! "@exec_prefix@", prefix
       s.gsub! "@libdir@", lib
       s.gsub! "@includedir@", include
-      s.gsub! "@SYNCTEXVERSION@", "0.0.0"
+      s.gsub! "@SYNCTEXVERSION@", "1.18.0"
     end
 
     mkdir "#{lib}/pkgconfig"
