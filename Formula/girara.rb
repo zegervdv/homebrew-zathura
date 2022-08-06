bb38f779256ade2e5a1226891899ed713cec25b1
Zeger Van de Vannet <747627+zegervdv@users.noreply.github.com>
2022-08-06 15:27:54 +0000

    girara: add 0.3.7 bottle.

--- Formula/girara.rb
+++ Formula/girara.rb
@@ -5,6 +5,12 @@ class Girara < Formula
   sha256 "41342213f8e745258f1db28cbb6ccc27a63009a5e001bf791bbe01ce436d4db7"
   head "https://github.com/pwmt/girara.git", branch: "develop"
 
+  bottle do
+    root_url "https://github.com/zegervdv/homebrew-zathura/releases/download/girara-0.3.7"
+    sha256 big_sur:      "edcefda742a6b7706e2c38a5fa09a5eb55e598b8f2e3edbe0de45647c1a2ac0f"
+    sha256 x86_64_linux: "a2537e075aa5e0b537bb4bc2efb821580f06766bd8cccefa3d2c0c6e0f96d96a"
+  end
+
   depends_on "cmake" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
