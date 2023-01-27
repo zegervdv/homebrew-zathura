de8d47466dbcedbcdacfe823e7f04f9ffc554e51
dakeryas <mrdakeryas@gmail.com>
2023-01-27 10:20:15 +0100

    Restore doc / manpage install

--- Formula/zathura.rb
+++ Formula/zathura.rb
@@ -29,11 +29,7 @@ class Zathura < Formula
     # Add the pkgconfig for girara to the PKG_CONFIG_PATH
     # TODO: Find out why it is not added correctly for Linux
     ENV["PKG_CONFIG_PATH"] = "#{ENV["PKG_CONFIG_PATH"]}:#{Formula["girara"].prefix}/lib/x86_64-linux-gnu/pkgconfig"
-
-    inreplace "meson.build" do |s|
-      s.gsub! "subdir('doc')", ""
-    end
-
+    
     mkdir "build" do
       system "meson", *std_meson_args, ".."
       system "ninja"
