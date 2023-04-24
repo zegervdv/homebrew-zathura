3e342df0af87257a7a3462216ea800f2b6afdc48
Zeger Van de Vannet <747627+zegervdv@users.noreply.github.com>
2023-04-24 13:16:08 +0200

    fix: remove failed bottles

--- Formula/girara.rb
+++ Formula/girara.rb
@@ -5,12 +5,6 @@ class Girara < Formula
   sha256 "24edfa3d493e7d4bbf491bb730d036cfedbd6c34ada1a7cfcd6273e78cffa44c"
   head "https://github.com/pwmt/girara.git", branch: "develop"
 
-  bottle do
-    root_url "https://github.com/zegervdv/homebrew-zathura/releases/download/girara-0.3.7"
-    rebuild 2
-    sha256 monterey:     "e937d11bdd14a93d178c90d0ae7a76d9a07ec8e02469d5adabc75094ebf9e185"
-    sha256 x86_64_linux: "e4f555c3f8da92f701a427a11f266722fd68c5ead0dac59b3e49992b37cdf9d3"
-  end
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
